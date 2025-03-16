f76cf7a2d895c248dd81a636c6aee1514c24c616
Karan Santosh Salunkhe <88623538+karansalunkhe21@users.noreply.github.com>
2025-03-15 21:04:23 -0400

    Update quality_check_silver.sql

--- tests/quality_check_silver.sql
+++ tests/quality_check_silver.sql
@@ -125,7 +125,7 @@ SELECT DISTINCT
     bdate 
 FROM silver.erp_cust_az12
 WHERE bdate < '1924-01-01' 
-   OR bdate > GETDATE();
+   OR bdate > NOW();
 
 -- Data Standardization & Consistency
 SELECT DISTINCT 
