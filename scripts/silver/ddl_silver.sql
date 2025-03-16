05a96badd584fd12dfeaaa144448241a4662bfdc
Karan Santosh Salunkhe <88623538+karansalunkhe21@users.noreply.github.com>
2025-03-15 20:47:36 -0400

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,14 @@
+/*
+===============================================================================
+DDL Script: Create Silver Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'silver' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 DROP TABLE IF EXISTS silver.crm_cust_info;
 
 create table silver.crm_cust_info(
@@ -7,7 +18,8 @@ cst_firstname varchar(50),
 cst_lastname varchar(50),
 cst_material_status varchar(50),
 cst_gndr varchar(20),
-cst_create_date date
+cst_create_date date,
+dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE silver.crm_cust_info  
@@ -22,7 +34,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_cost     INT,
     prd_line     VARCHAR(50),
     prd_start_dt TIMESTAMP,
-    prd_end_dt   TIMESTAMP
+    prd_end_dt   TIMESTAMP,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.crm_sales_details;
@@ -35,20 +48,23 @@ CREATE TABLE silver.crm_sales_details (
     sls_due_dt   INT,
     sls_sales    INT,
     sls_quantity INT,
-    sls_price    INT
+    sls_price    INT,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_loc_a101 ;
 CREATE TABLE silver.erp_loc_a101 (
     cid    VARCHAR(50),
-    cntry  VARCHAR(50)
+    cntry  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_cust_az12  ;
 CREATE TABLE silver.erp_cust_az12 (
     cid    VARCHAR(50),
     bdate  DATE,
-    gen    VARCHAR(50)
+    gen    VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2  ;
@@ -56,5 +72,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     id           VARCHAR(50),
     cat          VARCHAR(50),
     subcat       VARCHAR(50),
-    maintenance  VARCHAR(50)
+    maintenance  VARCHAR(50),
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
